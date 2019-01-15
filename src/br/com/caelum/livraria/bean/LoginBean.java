92cffd438a9ef6393b9b89cada5e25a8752bc117
Marcus Vinicius Rigonati <marcus.rigonati@neogrid.com>
2019-01-15 14:57:53 -0200

    13. Logout e o Escopo Flash

--- src/br/com/caelum/livraria/bean/LoginBean.java
+++ src/br/com/caelum/livraria/bean/LoginBean.java
@@ -2,6 +2,7 @@ package br.com.caelum.livraria.bean;
 
 import java.util.Map;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
@@ -23,24 +24,32 @@ public class LoginBean {
 		
 		System.out.println("Fazendo login - " + this.user.getEmail());
 		
+		FacesContext context = FacesContext.getCurrentInstance();
 		this.user = new UserDAO().getUserBy(this.user.getEmail(), this.user.getPassword());
 		
 		if (this.user != null) {
-			saveUserOnSession();
+			saveUserOnSession(context);
 			return "livro?faces-redirect=true";
 		}
-
-		return null;
+		
+		// Para manter esta mensagem para duas requisições
+		context.getExternalContext().getFlash().setKeepMessages(true);
+		context.addMessage(null, new FacesMessage("Usuário ou senha incorretos."));
+		
+		// A mensagem é perdida se não foi guardada por mais de uma requisição
+		return "login?faces-redirect=true";
 		
 	}
-
-	private void saveUserOnSession() {
-		
-		FacesContext context = FacesContext.getCurrentInstance();
+	
+	private void saveUserOnSession(FacesContext context) {
 		Map<String, Object> sessionMap = context.getExternalContext().getSessionMap();
-		
 		sessionMap.put("user", this.user);
-		
+	}
+	
+	public String logout() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.getExternalContext().getSessionMap().remove("user");
+		return "login?faces-redirect=true";
 	}
 	
 }
